c65c6fd7ae44758829b74584aa30dc6d0bbc2480
queuebit <queuebit10648@gmail.com>
2013-07-11 20:29:53 -0400

    Added route for '/'

--- db/migrate/20130711235434_create_users.rb
+++ db/migrate/20130711235434_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def up
+    create_table :users do |t|
+      t.string :name
+      t.timestamps
+    end
+    User.create(name: "RT")
+    User.create(name: "PG")
+    User.create(name: "BS")
+    User.create(name: "JH")
   end
 
   def down
+    drop_table :users
   end
 end
--- nai_warikan.rb
+++ nai_warikan.rb
@@ -6,3 +6,22 @@ set :database, "sqlite3:///nai_warikan.db"
 class User < ActiveRecord::Base
 
 end
+
+get "/" do
+  @users = User.order("name ASC")
+  erb :"users/index"
+end
+
+helpers do
+  def name
+    if @name
+      "#{@name} -- nai warikan"
+    else
+      "nai warikan"
+    end
+  end
+
+  def pretty_date(time)
+    time.strftime("%d %b %Y")
+  end
+end
